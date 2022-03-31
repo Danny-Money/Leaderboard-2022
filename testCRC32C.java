7dabd9e40d72d24f26af7f9b4d77f3192280b7f3
20019181 <danielt.money@gmail.com>
2022-03-31 19:48:21 -0400

    Removing extra files from git repo.

--- testCRC32C.java
+++
@@ -1,6 +0,0 @@
-public class testCRC32C {
-    public static void main(String[] args){
-        entryClass a = new entryClass("hello there",0,0,"c7bf21b1efab8a25b61fd0acff3c81571d42a81343fa5140f762bf92b76a026e");
-        System.out.println(a.getHashRec() + "," + a.getHashCom());
-    }
-}
